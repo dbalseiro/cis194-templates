e471c3ab3377c71cb28e4e66a7d8f56c2f97ee11
Diego Balseiro <diego.balseiro@syntagma.com.ar>
2017-08-17 00:09:54 -0500

    Week 5

--- homework05/src/Calc.hs
+++ homework05/src/Calc.hs
@@ -24,8 +24,14 @@ import qualified Data.Map as M
 -- >>> eval (ExprT.Mul (ExprT.Add (ExprT.Lit 2) (ExprT.Lit 3)) (ExprT.Lit 4)) == 20
 -- True
 
-eval :: ExprT -> Integer
-eval = undefined
+eval :: Maybe ExprT -> Maybe Integer
+eval Nothing = Nothing
+eval (Just e) = Just (eval' e)
+
+eval' :: ExprT -> Integer
+eval' (Lit x) = x
+eval' (Add e1 e2) = (eval' e1) + (eval' e2)
+eval' (Mul e1 e2) = (eval' e1) * (eval' e2)
 
 
 ----------------------------------------------------------------------
@@ -33,8 +39,7 @@ eval = undefined
 ----------------------------------------------------------------------
 
 evalStr :: String -> Maybe Integer
-evalStr = undefined
-
+evalStr = eval . parseExp Lit Add Mul
 
 ----------------------------------------------------------------------
 -- Exercise 3
@@ -48,6 +53,16 @@ evalStr = undefined
 reify :: ExprT -> ExprT
 reify = id
 
+class Expr expression where
+    lit :: Integer -> expression
+    mul :: expression -> expression -> expression
+    add :: expression -> expression -> expression
+
+instance Expr ExprT where
+    lit i = Lit i
+    mul e1 e2 = Mul e1 e2
+    add e1 e2 = Add e1 e2
+
 ----------------------------------------------------------------------
 -- Exercise 4
 ----------------------------------------------------------------------
@@ -55,13 +70,57 @@ reify = id
 newtype MinMax  = MinMax Integer deriving (Eq, Show)
 newtype Mod7    = Mod7 Integer deriving (Eq, Show)
 
+instance Expr Integer where
+    lit = id
+    mul = (*)
+    add = (+)
+
+instance Expr Bool where
+    lit = (>0)
+    mul = (&&)
+    add = (||)
+
+instance Expr MinMax where
+    lit i = MinMax i
+    mul = min
+    add = max
+
+instance Expr Mod7 where
+    lit i = Mod7 (i `mod` 7)
+    mul (Mod7 i) (Mod7 j) = lit (i * j)
+    add (Mod7 i) (Mod7 j) = lit (i + j)
+
 ----------------------------------------------------------------------
 -- Exercise 5 (do this OR exercise 6)
 ----------------------------------------------------------------------
 
-compile :: String -> Maybe Program
-compile = undefined
+data Token = Const Integer
+           | Plus Token Token
+           | Star Token Token
+  deriving Show
 
+class Expr expression where
+    lit :: Integer -> expression
+    mul :: expression -> expression -> expression
+    add :: expression -> expression -> expression
+
+-- Exercise 5
+instance Expr S.Program where
+    lit i = [PushI i]
+    add e1 e2 = e1 ++ e2 ++ [Add]
+    mul e1 e2 = e1 ++ e2 ++ [Mul]
+
+compile :: String -> Maybe Program
+compile = peval . parseExp Const Plus Star
+
+peval :: Maybe Token -> Maybe Program
+peval Nothing = Nothing
+peval (Just e) = Just (peval' e)
+
+peval' :: Token -> Program
+peval' (Const i) = lit i :: Program
+peval' (Plus e1 e2) = add (peval' e1) (peval' e2) :: Program
+peval' (Star e1 e2) = mul (peval' e1) (peval' e2) :: Program
 
 ----------------------------------------------------------------------
 -- Exercise 6 (do this OR exercise 5)
